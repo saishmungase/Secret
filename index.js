9f9e10d7e53b7da15e17e7d61ee4233df02414e9
Saish Mungase <saishmungase@gmail.com>
2025-01-11 21:09:04 +0530

    final Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    //This will run depends on the server is busy or not
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
